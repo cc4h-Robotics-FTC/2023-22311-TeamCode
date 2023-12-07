b5ffb195f51da2c86e236553f8e59c4eecc28a54
cc4hftc <91799027+cc4hftc@users.noreply.github.com>
2023-12-06 19:20:00 -0500

    Update Autonomous22311.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous22311.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous22311.java
@@ -7,21 +7,46 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 @Autonomous
 public class Autonomous22311 extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        //DistanceSensor Colores = hardwareMap.get(DistanceSensor.class, "Color");//
+
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+
 
         double DISTANCE = 60; // in
 
         waitForStart();
+        Random(40, 0);
 
-        Random(125,-90);
+        //double distance = Colores.getDistance(DistanceUnit.CM);//
+
+
+        /*if(distance<2.1){
+            telemetry.addData("Imhere", distance);
+        }
+        else{
+            Random(0, 90);
+            if(distance<2.1){
+                telemetry.addData("Imthere", distance);
+
+            }
+            else{
+                Random(0, 180);
+                telemetry.addData("Imzone", distance);
+            }
+        }*/
+
     }
 
     public double Random(double Xcoord, double ANGLE) {
